f5b7390becf0479a0a179f1648aade12e5a5d90e
Paul Karpukhin <ruberoid55@gmail.com>
2020-04-09 13:14:02 +0600

    rest client name was replaced with api

--- api/src/test/java/com/rv1den/schedule/rest_client/ExampleUnitTest.kt
+++ api/src/test/java/com/rv1den/schedule/rest_client/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.rv1den.schedule.rest_client
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
